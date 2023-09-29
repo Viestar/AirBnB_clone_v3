bf5fd17a77eb555566653742fff5f0155e186040
Viestar <m.silverbuckson@gmail.com>
2023-09-29 17:12:45 +0300

    pycodestyled added one more line after imports

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def return_status():
     """ Response for a successful fetch """
