1661813c6d5673f94de37dd6a064bd50fab9e7d2
Viestar <m.silverbuckson@gmail.com>
2023-09-29 16:23:18 +0300

    Status of the API - Task 3

---
+++ api/v1/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Flask Api Application """
+import os
+from models import storage
+from api.v1.views import app_views
+from flask import Flask
+
+app = Flask("__name__")
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def shut_and_clear_everything(exception):
+    """ Closes Database and clears everything """
+    storage.close()
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("HBNB_API_HOST", 5000))
+    host = os.getenv("HBNB_API_PORT", "0.0.0.0")
+    app.run(host=host, port=port, threaded=True)
