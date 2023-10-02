6e5d133e77222ea198aa49380b70cb06ff48c9c0
Viestar <m.silverbuckson@gmail.com>
2023-10-02 09:54:08 +0300

    Corrections

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-""" Flask running on port 5000 interactively with the storage """
-from models import storage
-from flask import Flask
-from flask import render_template
+"""
+starts a Flask web application
+"""
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Displays an HTML page  with states sorted by name and related cities"""
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
